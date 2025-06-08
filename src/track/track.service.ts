104f231e30b273b5deff6aa50de72935ebe05113
German Gribanov <112155209+GermanGrib@users.noreply.github.com>
2025-06-08 23:36:13 +0100

    feat: add prisma to track

--- src/track/track.service.ts
+++ src/track/track.service.ts
@@ -8,21 +8,22 @@ import { Track } from './entities/track.entity';
 import { CreateTrackDto } from './dto/create-track.dto';
 import { UpdateTrackDto } from './dto/update-track.dto';
 import { OnEvent } from '@nestjs/event-emitter';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class TrackService {
-  private tracks: Track[] = [];
+  constructor(private prisma: PrismaService) {}
 
-  findAll(): Track[] {
-    return this.tracks;
+  async findAll(): Promise<Track[]> {
+    return this.prisma.track.findMany();
   }
 
-  findById(id: string): Track {
+  async findById(id: string): Promise<Track> {
     if (!isValidUUID(id)) {
       throw new BadRequestException('Invalid UUID');
     }
 
-    const track = this.tracks.find((track) => track.id === id);
+    const track = await this.prisma.track.findUnique({ where: { id } });
     if (!track) {
       throw new NotFoundException('Track not found');
     }
@@ -30,7 +31,7 @@ export class TrackService {
     return track;
   }
 
-  create(createTrackDto: CreateTrackDto): Track {
+  async create(createTrackDto: CreateTrackDto): Promise<Track> {
     const newTrack: Track = {
       id: crypto.randomUUID(),
       name: createTrackDto.name,
@@ -39,67 +40,66 @@ export class TrackService {
       duration: createTrackDto.duration,
     };
 
-    this.tracks.push(newTrack);
+    await this.prisma.track.create({ data: newTrack });
+
     return newTrack;
   }
 
-  update(id: string, updateTrackDto: UpdateTrackDto): Track {
+  async update(id: string, updateTrackDto: UpdateTrackDto): Promise<Track> {
     if (!isValidUUID(id)) {
       throw new BadRequestException('Invalid UUID');
     }
 
-    const trackIndex = this.tracks.findIndex((track) => track.id === id);
-    if (trackIndex === -1) {
+    const track = await this.prisma.track.findUnique({ where: { id } });
+    if (!track) {
       throw new NotFoundException('Track not found');
     }
 
-    const updatedTrack = {
-      ...this.tracks[trackIndex],
-      ...updateTrackDto,
-      artistId:
-        updateTrackDto.artistId !== undefined
-          ? updateTrackDto.artistId
-          : this.tracks[trackIndex].artistId,
-      albumId:
-        updateTrackDto.albumId !== undefined
-          ? updateTrackDto.albumId
-          : this.tracks[trackIndex].albumId,
-    };
+    const updatedTrack = await this.prisma.track.update({
+      where: { id },
+      data: {
+        ...track,
+        ...updateTrackDto,
+        artistId:
+          updateTrackDto.artistId !== undefined
+            ? updateTrackDto.artistId
+            : track.artistId,
+        albumId:
+          updateTrackDto.albumId !== undefined
+            ? updateTrackDto.albumId
+            : track.albumId,
+      },
+    });
 
-    this.tracks[trackIndex] = updatedTrack;
     return updatedTrack;
   }
 
-  delete(id: string): void {
+  async delete(id: string): Promise<void> {
     if (!isValidUUID(id)) {
       throw new BadRequestException('Invalid UUID');
     }
 
-    const trackIndex = this.tracks.findIndex((track) => track.id === id);
-    if (trackIndex === -1) {
+    const track = await this.prisma.track.findUnique({ where: { id } });
+    if (!track) {
       throw new NotFoundException('Track not found');
     }
 
-    this.tracks.splice(trackIndex, 1);
+    await this.prisma.track.delete({ where: { id } });
   }
 
   @OnEvent('artist.deleted')
-  handleArtistDeleted(artistId: string) {
-    this.tracks = this.tracks.map((track) => {
-      if (track.artistId === artistId) {
-        return { ...track, artistId: null };
-      }
-      return track;
+  async handleArtistDeleted(artistId: string) {
+    await this.prisma.track.updateMany({
+      where: { artistId },
+      data: { artistId: null },
     });
   }
 
   @OnEvent('album.deleted')
-  handleAlbumDeleted(albumId: string) {
-    this.tracks = this.tracks.map((track) => {
-      if (track.albumId === albumId) {
-        return { ...track, albumId: null };
-      }
-      return track;
+  async handleAlbumDeleted(albumId: string) {
+    await this.prisma.track.updateMany({
+      where: { albumId },
+      data: { albumId: null },
     });
   }
 }
