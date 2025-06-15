35cfd2de0ae0037c359768394ccfd7e4174ea7d6
German Gribanov <112155209+GermanGrib@users.noreply.github.com>
2025-06-15 17:00:32 +0100

    feat: add to app module & upd env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,22 @@
-import { Module, ValidationPipe } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  ValidationPipe,
+} from '@nestjs/common';
+import { APP_FILTER, APP_PIPE } from '@nestjs/core';
+import { ConfigModule } from '@nestjs/config';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+
 import { UserModule } from './user/user.module';
 import { ArtistModule } from './artist/artist.module';
 import { TrackModule } from './track/track.module';
 import { AlbumModule } from './album/album.module';
-import { EventEmitterModule } from '@nestjs/event-emitter';
-import { ConfigModule } from '@nestjs/config';
 import { FavoritesModule } from './favorites/favorites.module';
-import { APP_PIPE } from '@nestjs/core';
 import { PrismaModule } from './prisma/prisma.module';
+import { LoggingService } from './logging/logging.service';
+import { AllExceptionsFilter } from './filters/all-exceptions.filter';
+import { RequestLoggingMiddleware } from './middleware/request-logging.middleware';
 
 @Module({
   imports: [
@@ -21,10 +30,19 @@ import { PrismaModule } from './prisma/prisma.module';
     FavoritesModule,
   ],
   providers: [
+    LoggingService,
     {
       provide: APP_PIPE,
       useClass: ValidationPipe,
     },
+    {
+      provide: APP_FILTER,
+      useClass: AllExceptionsFilter,
+    },
   ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(RequestLoggingMiddleware).forRoutes('*');
+  }
+}
